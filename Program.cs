75052d0a55aee7635abdc73bc2a0f5e173d77f81
Saif Kamal Chowdhury <saifx2y@gmail.com>
2022-01-05 00:46:26 +0600

    all files

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Diagnostics;
+using IronPython.Hosting;
+using Microsoft.Scripting.Hosting;
+namespace RunPythonConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //RunPythonWithIronPy();
+            RunPythonWithBashLinux();
+        }
+        private static void RunPythonWithIronPy()
+        {
+            ScriptEngine engine = Python.CreateEngine();
+            engine.ExecuteFile(@"test.py");
+        }
+        private static void RunPythonWithBashLinux()
+        {
+            var resp = RunCommandWithBashLinux("python3  /var/www/testpythoncode/test.py");
+            Console.WriteLine(resp);
+        }
+        private static string RunCommandWithBashLinux(string command)
+        {
+            var psi = new ProcessStartInfo();
+            psi.FileName = "/bin/bash";
+            psi.Arguments = command;
+            psi.RedirectStandardOutput = true;
+            psi.UseShellExecute = false;
+            psi.CreateNoWindow = true;
+
+            using var process = Process.Start(psi);
+
+            process.WaitForExit();
+
+            var output = process.StandardOutput.ReadToEnd();
+
+            return output;
+        }
+    }
+}
